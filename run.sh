384a19dac61fdcc8564ec22e98abb8d18d559a93
satyapraneet63 <satyapraneet63@gmail.com>
2024-03-15 22:55:22 +0530

    test-1

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t quote_server:$tag .
+docker build -t quote_server:$tag /home/praneet/personal/typeface-assignment/
 kubectl apply -f Kubernetes/deployment.yaml
 kubectl apply -f Kubernetes/service.yaml
