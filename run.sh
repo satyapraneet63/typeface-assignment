965234373c8d7426f5ec5b5305d0d5df92e4ca4a
Satya Praneet <114915246+satyapraneet63@users.noreply.github.com>
2024-03-16 00:08:56 +0530

    added a new workflow to trigger run.sh (#28)

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t quote_server:$tag .
-kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
+docker build -t quote_server:$TAG_VERSION .
+kubectl apply -f Kubernetes/deployment.yaml
+kubectl apply -f Kubernetes/service.yaml
